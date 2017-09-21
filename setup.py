1392304fa2655ca1603184d109063fcb4f779083
zhang <dadamail@foxmail.com>
2017-09-21 22:01:35 +0800

    🌈text_filter支持已编译的正则，当存储器不存在时会设置默认sqlite3存储器。💧

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = [
 setuptools.setup(
     name='weixin',
     packages=setuptools.find_packages(),
-    version="0.1.1",
+    version="0.1.2",
     description="一个Python跨网络框架的小型微信公众号开发包。",
     author="Zhang Yi Da",
     url="https://github.com/thisforeda/weixin-Python-SDK",
