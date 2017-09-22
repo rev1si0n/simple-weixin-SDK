3fde0968222e8024e51ef27e3fc7d078a16ea6b2
thisforeda <thisforeda@live.cn>
2017-09-22 08:13:32 +0800

    重命名server_api为client_api，requirements从文件读取🍓

--- setup.py
+++ setup.py
@@ -1,20 +1,11 @@
 import setuptools
 
-requirements = [
-    'IPy>=0.83',
-    'msgpack-python>=0.4.8',
-    'pycrypto>=2.6.1',
-    'requests>=2.18.4',
-    'xmltodict>=0.11.0',
-    'PyMySQL>=0.7.11',
-    'redis>=2.10.6'
-    ]
-
+requirements = open('./requirements.txt').readlines()
 
 setuptools.setup(
     name='weixin',
     packages=setuptools.find_packages(),
-    version="0.1.2",
+    version="0.1.3",
     description="一个Python跨网络框架的小型微信公众号开发包。",
     author="Zhang Yi Da",
     url="https://github.com/thisforeda/weixin-Python-SDK",
