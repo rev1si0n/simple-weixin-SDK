f00ddecc9e6bae0800089a2befe47f0241ff3656
thisforeda <thisforeda@live.cn>
2017-10-22 10:54:12 +0800

    🚀去除了CLICK事件子路由，将其放置到主路由中。👹

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ requirements = open('./requirements.txt').readlines()
 setuptools.setup(
     name='weixin',
     packages=setuptools.find_packages(),
-    version="0.1.4 (20171003)",
+    version="0.1.4 (20171022)",
     description="一个Python跨网络框架的小型微信公众号开发包。",
     author="Zhang Yi Da",
-    url="https://github.com/thisforeda/weixin-Python-SDK",
+    url="https://github.com/500-Error/weixin-SDK",
     license='MIT',
     platforms=['any'],
     install_requires=requirements,
