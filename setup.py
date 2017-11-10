91fe1cdb056023b1a99383a2503b08db002f6817
thisforeda <thisforeda@live.cn>
2017-11-10 12:26:45 +0800

    update.🎯

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ requirements = open('./requirements.txt').readlines()
 setuptools.setup(
     name='weixin',
     packages=setuptools.find_packages(),
-    version="0.1.5.20171102",
+    version="0.1.5.20171110",
     description="一个Python跨网络框架的小型微信公众号开发包。",
     author="Zhang Yi Da",
     url="https://github.com/LUSG/weixin-SDK",
