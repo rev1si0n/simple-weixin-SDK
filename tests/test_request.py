2b01414ccb1ee5699c82d0d8fdcd9f8f7e2c7790
thisforeda <thisforeda@live.cn>
2017-11-14 18:58:51 +0800

    fix problem in Python3.4.🤢

--- tests/test_request.py
+++ tests/test_request.py
@@ -27,14 +27,16 @@ text_msg_encrypted = """
     <Encrypt><![CDATA[ROqwv6GlhGmZfli33Ub6Kka3K2YHP/yJXQGYb2sYX1cCFZHB0RztThxWrYEcmuyhfptueEE9+6/s3XVB/3UYyDPXy6QIyCX1WW5guCoMeRCH2B5vO7/rsWrPVkxieu6VXHhS9fBHgL9HB38OlNiGn+lheU7N3+XefuM53b5CO3w51xYvOhadGGBOzM4+83Xlp5jdaA/GveEmsDdPyCH1tF5/bxdwFRbqYNGO4DXtmtOv+XexSTpPHorAiDcOGMoLdFcF5xBX3cPd9YOjIk0rZ7n0NZpyEA8s6v1AXuzS3KYxO+u3rblR+z+hK5EOypmP02cWFuR8F7rzyJJnaRvqYeGsr8yOJCq6wXoiyDvkIYBW2951AV07rs0JcNwJYy0I/t1ZqZQaIfY5XMTJfEltjzu/u9i4uAyrOYAna8Ozhcj0a3P8j7BSN/QTiCXrrAfyOiOEJYURl9iBM4LZTRiZkg==]]></Encrypt>
 </xml>
 """
+enc_cfg = common_cfg.copy()
+enc_cfg['cryptor'] = XMLMsgCryptor(
+    token='A'*20,
+    appid='wx' + 'a'*16,
+    enc_aeskey='E5HSDyJ78YwCSelWbCSFb4ZcXXSN1LQPdkHPblR8ilo'
+    )
 
 @pytest.mark.parametrize("config,umsg", [
     (Config(**common_cfg), text_msg),
-    (Config(**common_cfg, cryptor=XMLMsgCryptor(
-        **common_cfg,
-        enc_aeskey='E5HSDyJ78YwCSelWbCSFb4ZcXXSN1LQPdkHPblR8ilo'
-        )
-    ), text_msg_encrypted),
+    (Config(**enc_cfg), text_msg_encrypted),
 ])
 def test_request(config, umsg):
 
